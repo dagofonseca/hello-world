649480a025a18d36be8598cf7f8568faaf2e0834
dagofonseca <daafonsecara@unal.edu.co>
2018-05-23 20:20:29 -0500

    Intento despliegue sin build

---
+++ hello-app/src/Prueba.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Prueba extends Component {
+  render() {
+    return (
+      <div>
+        <h1>vamos a ver si sirve?</h1>
+      </div>
+    );
+  }
+}
+
+export default Prueba;
